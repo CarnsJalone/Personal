af7710ec9a7ff65174c2dd3911bf68aa1ad6a23e
CarnsJalone <Jlamborghini22@gmail.com>
2019-01-08 15:42:48 -0500

    javascript search engine in progress

--- Personal/static/js/pdf_parser.js
+++ Personal/static/js/pdf_parser.js
@@ -10,15 +10,83 @@ $(document).ready(function(){
 
     function search() {
 
+
         var data = $data.html()
 
-        console.log(typeof($data))
+        data = data.replace("<p>", "").replace("</p>", "").replace(/[.\w\d\s]*-<\/p><p>[.\w\d\s]*/g, "\n").replace(/\n/ig, '')
+
+        var each_line = data.split("<br>")
+
+        // console.log(each_line)
+
+        for (i=0; i<each_line.length; i++) {
+
+            var trimmed_lines = each_line[i].split(" ")
+
+            // trimmed_lines.innerHTML = "<p>" + trimmed_lines + "</p>"
+
+            for (j=0; j<trimmed_lines.length; j++) {
+
+                seperated_words = trimmed_lines[j].trim()
+
+                // seperated_words = seperated_words.toUpperCase()
+
+                if ($input.val().length >=3) {
+
+                    var substring = seperated_words.includes($input.val().toUpperCase())
+
+                    if (substring) {
+
+
+        
+ 
+                    }
+                }
+            }
+
+        }
+
+        //     for (j=0; j<trimmed_lines.length; j++) {
+
+
+        //         seperated_words = trimmed_lines[j].trim()
+
+        //         seperated_words = seperated_words.toUpperCase()
+
+        //         if ($input.val().length >=3) {
+
+        //             var substring = seperated_words.includes($input.val().toUpperCase())
+
+        //             if (substring) {
+
+        //                 console.log(data[trimmed_lines])
+
+        //                 // substring.html("<span id='immediate_search_terms'" + data[substring] + "></span>")
+
+        //                 // Highlight the actual words searched
+        //                 // substring.innerHTML(substring + "<id=immediate_search_terms>")
+
+        //                 console.log(trimmed_lines)
+        //             }
+
+        //         }
+
+        //     }
+
+
+        }
+
+        // console.log(data)
+
+        // console.log(each_line)
+
+        // console.log(data)
 
         // for (i=0; i<data.length; i++) {
         //     console.log(data[i])
         //     console.log(typeof(data))
         // }
 
-    }
+
 
 })
