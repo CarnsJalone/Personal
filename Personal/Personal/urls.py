2ec5fef122b0b14cb9f9a66214cbcd5c40e20646
CarnsJalone <Jlamborghini22@gmail.com>
2019-01-19 11:03:40 -0500

    creating custom bad request views

--- Personal/Personal/urls.py
+++ Personal/Personal/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler400, handler403, handler404, handler500
 from django.conf.urls.static import static
 # from django.conf import settings
 
